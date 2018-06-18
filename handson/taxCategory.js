5cf6b5e683091b7b2411da02bb9fe3bbc6ddf64c
Evan Bonertz <evan.bonertz@commercetools.com>
2018-06-18 12:10:30 -0400

    Return Tax Category complete

--- handson/taxCategory.js
+++ handson/taxCategory.js
@@ -3,9 +3,19 @@ const sdkRequestBuilder = require('@commercetools/api-request-builder');
 const { getClient, projectKey } = require('./client.js');
 
 const getTaxCategories = function getTaxCategories() {
-  // TODO: 3.6
-  // Get a list of product types
-  
+
+  return getClient().then((client) => {
+
+    const requestBuilder = sdkRequestBuilder.createRequestBuilder({ projectKey });
+    const taxCategoryUri = requestBuilder.taxCategories.build();
+    const taxCategoryRequest = {
+      uri: taxCategoryUri,
+      method: 'GET'
+    };
+    return client.execute(taxCategoryRequest);
+
+  });
+
 };
 
 module.exports.getTaxCategories = getTaxCategories;
